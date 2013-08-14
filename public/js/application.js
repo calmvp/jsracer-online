e74421e41188c8e2e42dfeca9b4b7deffe720dc1
Ned KY <nkaidayip@gmail.com>
2013-08-13 20:36:04 -0500

    object oriented movement and object creation completed

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,47 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+function Player(name, track_id){
+  this.name = name;
+  this.track_id = track_id
+};
+
+function Game(player1, player2){
+	this.player1 = player1;
+	this.player2 = player2
+};
+
+Game.prototype.checkFinished = function(){
+
+};
+
+Game.prototype.movePlayer = function(player){
+  var active = player.track_id + " .active";
+  var next = $(active).next();
+  $(active).removeClass('active');
+  $(next).addClass('active')
+};
+
+
+$(document).ready(function(){
+
+  var player1 = new Player($('#player1name').text(), "#player1");
+  var player2 = new Player($('#player2name').text(), "#player2");
+  var game = new Game(player1, player2)
+
+  console.log(player1)
+  console.log(player2)
+  console.log(game)
+
+  raceStart = new Date();
+
+   $(this).keyup(function(event){
+
+      if (event.keyCode == 81){
+      	console.log('player1 moving')
+    	game.movePlayer(player1)
+      }
+      else if (event.keyCode == 80){  
+      	console.log('player2 moving')
+    	game.movePlayer(player2)  
+  	  }
+  });
 });
--- public/js/racer.js
+++ public/js/racer.js
@@ -1,62 +1,76 @@
-$(document).ready(function(){
-  raceStart = new Date();
+// $(document).ready(function(){
+
+//   var player1 = new Player($('#player1name').text(), "#player1 td");
+//   var player2 = new Player($('#player2name').text(), "#player2 td");
+//   var game = new Game(player1, player2)
+
+//   console.log(player1)
+//   console.log(player2)
+//   console.log(game)
+
+//   raceStart = new Date();
   
   //instantiate objects. players, game. 
   //game contains -- racetime, winner, loser, board position
+  //var player1 = new Player($(#player1id).inner_text());
 
-  console.log(raceStart);
 
-  $(this).keyup(function(event){
-    if (event.keyCode == 81){
+// });
 
-      $("#player1 td.active").removeClass('active').next().addClass('active');
-    }
-    else if (event.keyCode == 80){
-      $("#player2 td.active").removeClass('active').next().addClass('active');
-    }
-    if ( $('tr td:last-child').hasClass('active') )
-    {   
-      raceFinish = new Date();
-      raceTime = Math.abs(raceFinish - raceStart);
-      $(this).unbind('keyup');
-      var p1 = $("#player1name").text();
-      var p2 = $("#player2name").text();
-      if ($('tr:first-child td:last-child' ).hasClass('active'))
-        { 
-          alert(p1 + ' WINS!! In '  + raceTime / 1000.0 + ' seconds!');
-          var data = {
-            raceTime : raceTime,
-            winner : p1,
-            loser : p2
-          };
+  // $(this).keyup(function(event){
+    
 
-          var params = $.param(data)
+  //   if (event.keyCode == 81){
+  //     //move player1 function
+  //     $("#player1 td.active").removeClass('active').next().addClass('active');
+  //   }
+  //   else if (event.keyCode == 80){
+  //     //move player2 function
+  //     $("#player2 td.active").removeClass('active').next().addClass('active');
+//   //   }
+//     if ( $('tr td:last-child').hasClass('active') )
+//     {   
+//       raceFinish = new Date();
+//       raceTime = Math.abs(raceFinish - raceStart);
+//       $(this).unbind('keyup');
+//       var p1 = $("#player1name").text();
+//       var p2 = $("#player2name").text();
+//       if ($('tr:first-child td:last-child' ).hasClass('active'))
+//         { 
+//           alert(p1 + ' WINS!! In '  + raceTime / 1000.0 + ' seconds!');
+//           var data = {
+//             raceTime : raceTime,
+//             winner : p1,
+//             loser : p2
+//           };
 
-          // console.log($params)
-          $.post('/victory', data, function(response)
-            {
-             $('body').html(response);
+//           var params = $.param(data)
+
+//           // console.log($params)
+//           $.post('/victory', data, function(response)
+//             {
+//              $('body').html(response);
              
-            });
+//             });
         
-        }
-      else
-        {
-          alert(p2 + ' WINS!! In ' +  raceTime / 1000.0 + ' seconds!');
-          var data = {
-            raceTime : raceTime,
-            winner : p2,
-            loser : p1
-          };
+//         }
+//       else
+//         {
+//           alert(p2 + ' WINS!! In ' +  raceTime / 1000.0 + ' seconds!');
+//           var data = {
+//             raceTime : raceTime,
+//             winner : p2,
+//             loser : p1
+//           };
 
-          var params = $.param(data)
-          $.post('/victory', data, function(response)
-          {
-            $('body').html(response)          
-          });
+//           var params = $.param(data)
+//           $.post('/victory', data, function(response)
+//           {
+//             $('body').html(response)          
+//           });
       
-        }
-    }
-  });
+//         }
+//     }
+//   });
 
-});
+// });
